ea643c3294e9806401a81ff61fc70853c21021fb
Domenico Alessandria <domenico3luiss@gmail.com>
2023-01-21 18:11:14 +0100

    added methods and condition exercises

---
+++ Python/Exercises/Part1/methods-2.py
@@ -0,0 +1,9 @@
+num1 = float(1.3)
+num2 = int(2.3)
+num3 = complex(1j) 
+num4 = round(1.4) 
+num5 = round(1.5)
+
+print(num1,num2,num3,num4,num5)
+print(type(num1),type(num2),type(num3),type(num4),type(num5))
+
---
+++ Python/Exercises/Part1/methods-3.py
@@ -0,0 +1,13 @@
+num1 = 1122334455666
+num1_str=str(num1)
+len(num1_str)
+_3th=num1_str[2]
+print(_3th)
+_3th_to_5th= num1_str[2:5]
+print(_3th_to_5th)
+print(str(num2) in _3th)
+print(str(num3) in _3th)
+string_with_0='0'+num1_str
+print(string_with_0[0:3])
+print(string_with_0[4:])
+print(string_with_0[-3:])
