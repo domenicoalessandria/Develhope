74a3b9bb466b021c4fb8a3a590b7c8c849c63a15
Domenico Alessandria <domenico3luiss@gmail.com>
2023-02-13 21:35:34 +0100

    added last two class exs

--- Python/Exercises/Part2/all_classes.py
+++ Python/Exercises/Part2/all_classes.py
@@ -17,3 +17,41 @@ class animal():
 
 dog=animal(4)
 dog.n_leg
+
+
+## ex3
+class Animal():
+    
+    print('Animal object was created')
+    
+    def __init__(self,n_leg):
+        self.n_leg=n_leg
+        
+    def runs(self):
+        print('Running started')
+    
+    def _count_legs(self):
+        print(self.n_leg)
+        
+    def return_legs(self):
+        return self.n_leg
+    
+    def num_legs_plus_one(self):
+        return self.return_legs()+1
+
+## ex4
+
+class Dog(Animal):
+    
+    def __init__(self,n_leg,name):
+        Animal.__init__(self,n_leg)
+        self._name=name
+    
+    def bark(self):
+        print('Woof Woof')
+
+dog=Dog(4,'Barney')
+
+print(dog._name)
+dog.bark()
+dog._count_legs()
